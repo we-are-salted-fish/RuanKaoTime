7cd617aa26baa3bd48a4f178a7cbb8c57a723fe0
DanishBread <ismatch@qq.com>
2021-03-17 12:55:31 +0800

    first commit

---
+++ DanishBread.GetRunKao/MailHelper.cs
@@ -0,0 +1,37 @@
+﻿using DanishBread.GetRunKao.Config;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace DanishBread.GetRunKao
+{
+    public class MailHelper
+    {
+
+        public static void SendMail(MailConfig config, string msg)
+        {
+            MailAddress mailAddress = new MailAddress(config.Address);
+            MailMessage mailMessage = new MailMessage();
+
+            foreach (var item in config.ReceiveList)
+            {
+                if (!string.IsNullOrEmpty(item))
+                    mailMessage.To.Add(item.ToString());
+            }
+            
+            mailMessage.From = mailAddress;
+            mailMessage.Subject = "FuckWayne";
+            mailMessage.SubjectEncoding = Encoding.UTF8;
+            mailMessage.Body = msg;
+            mailMessage.BodyEncoding = Encoding.Default;
+            mailMessage.Priority = MailPriority.High;
+            mailMessage.IsBodyHtml = false;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new System.Net.NetworkCredential(config.Address, config.Password);
+            smtp.Host = config.Host;
+            smtp.Send(mailMessage);
+        }
+    }
+}
---
+++ DanishBread.GetRunKao/Program.cs
@@ -0,0 +1,89 @@
+﻿using AngleSharp;
+using DanishBread.GetRunKao.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DanishBread.GetRunKao
+{
+    class Program
+    {
+        private static MailConfig _mailConfig;
+        private static string _baseDir;
+
+        static void Init()
+        {
+            Type type = (new Program()).GetType();
+
+            _baseDir = Path.GetDirectoryName(type.Assembly.Location);
+            _mailConfig =
+               NewLife.Serialization.JsonHelper.ToJsonEntity<MailConfig>(
+                   File.ReadAllText(Path.Combine(_baseDir, "Config", "Mail.json")));
+        }
+
+        static async Task<List<TimeList>> Analysis()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var address = "https://bm.ruankao.org.cn/sign/welcome";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(address);
+            var cellSelector = "div.selectorg";
+            var cells = document.QuerySelectorAll(cellSelector);
+
+            var timeList = new List<TimeList>();
+
+            foreach (var cell in cells)
+            {
+                var ProvinceName = cell.ChildNodes[0].TextContent;
+                var TimeRange = cell.ChildNodes[1].TextContent;
+
+                timeList.Add(new TimeList { ProvinceName = ProvinceName, TimeRange = TimeRange });
+            }
+
+            return timeList;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Init();
+            //var r = GetHtmlString();
+            //Console.WriteLine(r);
+            //MailHelper.SendMail(_mailConfig, "广东的软考报名时间出来了。");
+            var flag = true;
+            while (flag)
+            {
+                var rList = Analysis().Result;
+
+                foreach (var item in rList)
+                {
+                    Console.WriteLine($"省份：{item.ProvinceName}   时间：{item.TimeRange}");
+                }
+
+                if (rList.Select(p => p.ProvinceName).Contains("广东"))
+                {
+                    MailHelper.SendMail(_mailConfig, "广东的软考报名时间出来了。");
+                    flag = false;
+                }
+
+                Thread.Sleep(60 * 1000);
+            }            
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+    public class TimeList
+    {
+        public string ProvinceName { get; set; }
+
+        public string TimeRange { get; set; }
+    }
+}
